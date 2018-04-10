701c2c703ce57aa88c01697dc2c066ed278b7938
NTDLS <josh@ntdls.com>
2018-04-09 21:32:19 -0400

    Add files via upload

---
+++ Resources/resource.h
@@ -0,0 +1,92 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by AutoUpdate.rc
+//
+#define IDI_MAINICON                    101
+#define IDI_MAIN                        101
+#define IDD_MAIN                        102
+#define IDD_CHAT                        102
+#define IDR_CHATDLGMENU                 103
+#define IDD_MASTERPW                    103
+#define IDD_MEMBERPW                    104
+#define IDD_SECURITYTAB                 104
+#define IDD_USERNAMEPW                  105
+#define IDD_ADVANCEDTAB                 105
+#define IDI_ADVANCEDTAB                 106
+#define IDD_GENERALTAB                  107
+#define IDI_GENERALTAB                  107
+#define IDI_SECURITYTAB                 108
+#define IDB_AWAY                        117
+#define IDB_HOST                        118
+#define IDB_LOCALUSER                   126
+#define IDB_REMOTEUSER                  127
+#define IDI_TRAYICON                    129
+#define IDR_MAINMENU                    133
+#define IDB_SPLASH                      134
+#define IDD_SPLASH                      135
+#define IDD_SQLTAB                      136
+#define IDI_SQLTAB                      136
+#define IDS_TITLECAPTION                138
+#define IDS_FILEVERSION                 139
+#define IDS_APPCOPYRIGHT                140
+#define IDS_REGISTRYKEY                 141
+#define IDS_STRING142                   142
+#define IDS_COMREGISTRYKEY              142
+#define IDS_UPDATEURL                   142
+#define IDR_USERLISTMENU                500
+#define IDC_TEXTBOX                     1000
+#define IDC_OK                          1001
+#define IDC_CANCEL                      1002
+#define IDC_STATICTEXT                  1003
+#define IDC_SENDEDIT                    1004
+#define IDC_RECVLIST                    1005
+#define IDC_SENDBUTTON                  1006
+#define IDC_COMPANYPWD                  1007
+#define IDC_TCPLOOPDELAY                1008
+#define IDC_COMPANYNAME                 1009
+#define IDC_PORTALSVR                   1010
+#define IDC_PORTALSVRPORT               1011
+#define IDC_USERNAME                    1012
+#define IDC_PASSWORD                    1013
+#define IDC_OKBUTTON                    1014
+#define IDC_CANCELBUTTON                1015
+#define IDC_USERLIST                    1017
+#define IDC_AUTHSTRING                  1018
+#define IDC_BASECOMMKEY                 1019
+#define IDC_LOCALCRYPTKEY               1020
+#define IDC_ADMINPASSWORD1              1021
+#define IDC_ADMINPASSWORD2              1022
+#define IDC_SPLASHPROGRESS              1023
+#define IDC_SERVER                      1024
+#define IDC_SQLDRIVER                   1025
+#define IDC_SQLDATABASE                 1026
+#define IDC_SQLUSERNAME                 1027
+#define IDC_SQLPASSWORD                 1028
+#define IDC_CONNECTINTERVAL             1029
+#define IDC_PROGRESSBAR                 1030
+#define IDC_STATUSTEXT                  1031
+#define IDC_APPTITLE                    1032
+#define IDC_BUTTON1                     1033
+#define ID_CONNECTION_SETTINGS          40001
+#define ID_CONNECTION_CONNECT           40002
+#define ID_CONNECTION_DISCONNECT        40003
+#define ID_USERLISTMENU_TALKINPRIVATE   40004
+#define ID_USERLISTMENU_KICKUSER        40005
+#define ID_USERLISTMENU_BANUSER         40006
+#define ID_CONNECTION_CONNECTNOW        40012
+#define ID_CONNECTION_FORCEDISCONNECT   40013
+#define ID_SETTINGS_GENERALSETTINGS     40014
+#define ID_ADVANCED_DEBUGMODE           40016
+#define ID_ADVANCED_OPENCONSOLE         40017
+#define ID_ADVANCED_FLUSHVARIABLES      40018
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        139
+#define _APS_NEXT_COMMAND_VALUE         40019
+#define _APS_NEXT_CONTROL_VALUE         1034
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
